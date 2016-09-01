36f671b47fda6914531df95a0d641106f387c48d
YuSong <971034623@qq.com>
2016-09-01 16:12:58 +0800

    Initial Commit

---
+++ archiveRuntime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  archiveRuntime
+//
+//  Created by  椒徒科技 on 16/9/1.
+//  Copyright © 2016年 jiaotukeji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
